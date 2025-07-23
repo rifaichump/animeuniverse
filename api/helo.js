a5f8c8ed44615fd6b0c22f48ad5abca2056cc3a6
rifaichump <78413935+rifaichump@users.noreply.github.com>
2025-07-23 20:10:37 +0700

    Delete api directory

--- api/helo.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).send('Hello');
-    console.log(res);
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
