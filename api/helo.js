e2e582094198fef197ecfcff0028d198121635f6
rifaichump <78413935+rifaichump@users.noreply.github.com>
2025-07-23 20:08:18 +0700

    Add files via upload

--- api/helo.js
+++ api/helo.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   if (req.method === 'GET') {
     res.status(200).send('Hello');
+    console.log(res);
   } else {
     res.status(405).send('Method Not Allowed');
   }
