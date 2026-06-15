903e191b4d85c40a17c81f02795009841c7ee936
rifaichump <78413935+rifaichump@users.noreply.github.com>
2026-06-15 11:58:30 +0700

    Add files via upload

---
+++ api/download.js
@@ -0,0 +1,27 @@
+function decode(text) {
+  return Buffer.from(text, "base64url").toString();
+}
+export default async function handler(req, res) {
+  const { id } = req.query;
+  if (!id) {
+    res.status(404).json({
+      status: false,
+      msg: "No url directed"
+    });
+  }
+  try {
+    const idd = decode(id);
+    if (!/animeunicraft/.test(idd)) {
+      res.status(404).json({
+        status: false,
+        msg: "No url directed"
+      });
+    }
+    res.redirect(url);
+  } catch (e) {
+    res.status(404).json({
+      status: false,
+      msg: "No url directed"
+    });
+  }
+}
