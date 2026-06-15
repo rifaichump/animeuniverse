e3c45dc1529939134fd443172809f7124921c994
rifaichump <78413935+rifaichump@users.noreply.github.com>
2026-06-15 11:46:43 +0700

    Delete api/key.js

--- api/key.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const remaining = 86400000;
-  res.status(200).json({
-    "ok": true,
-    "expiryMs": new Date() * 1 + remaining,
-    "remainingMs": remaining
-  });
-}
