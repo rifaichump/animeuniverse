29620dcb75247ecd3792fcb1e845357379b0124a
rifaichump <78413935+rifaichump@users.noreply.github.com>
2026-07-31 01:16:36 +0700

    Add files via upload

--- api/watch.js
+++ api/watch.js
@@ -392,7 +392,7 @@ const ICON_FS_EXPAND = '<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v
 const ICON_FS_COMPRESS = '<path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>';
 
 // Standard resolusi pertama
-const defaultReso = data.reso[0];
+const defaultReso = data.reso[data.reso.length - 1];
 video.src = data.streams[defaultReso][0].link;
 
 // ===== Volume Controls =====
